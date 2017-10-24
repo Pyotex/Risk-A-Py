50a9fb3dd58a78dbe0b3ba76e9567c02543637f7
Pyotex <pera.peki27@gmail.com>
2017-10-25 00:00:06 +0200

    Added wins count.

--- simulation.py
+++ simulation.py
@@ -1,8 +1,10 @@
-from utility import registry as reg, logging
+from utility import registry as reg, logging, graphs
 from game import Game
 import time
 
 start = time.time()
+
+wins = [0 for i in range(reg.player_count)]
 
 for i in range(0, reg.max_repetition):
     game = Game()
@@ -12,8 +14,13 @@ for i in range(0, reg.max_repetition):
         print(repr(players[0]) + " won after " + str(moves) + " moves!")
         logging.writeline(repr(players[0]) + " won after " + str(moves) + " moves!")
 
+        wins[players[0].number] += 1
+
     else:
         for player in players:
             print(repr(player) + " has " + str(len(player.territories)) + " territories")
 
+print(str(wins))
+print('Finished after: ' + str(time.time() - start))
+logging.writeline(str(wins))
 logging.closeFile()
